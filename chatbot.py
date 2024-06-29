5bb96d3d5074241f0adfee3a43a55b80e9ea3d7b
armandossrecife <armando.sousa@gmail.com>
2024-06-29 11:41:49 -0300

    Refatoracao para separar a configuracao do modelo da configuracao do chat

--- chatbot.py
+++ chatbot.py
@@ -8,23 +8,33 @@
 import google.generativeai as genai
 import os
 import sys
+from google.generativeai import GenerationConfig
 
 qtd_tokens_enviados = []
 qtd_tokens_recebidos = []
 
-def configura_modelo(modelo='gemini-1.0-pro-latest'): 
+def configura_modelo(modelo='gemini-1.0-pro-latest', max_token=4096, my_temperature=0.1): 
     try:
         # Configure o acesso ao Google Gemini
-        genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+        genai.configure(api_key=os.environ["GOOGLE_API_KEY"])        
+        # Generation Config
+        my_generation_config = GenerationConfig(max_output_tokens=max_token, temperature=my_temperature, top_p=1, top_k=32)
         # Defina o modelo de LLM
-        model = genai.GenerativeModel(modelo)
-        chat = model.start_chat(history=[])
-        return chat, model
+        model = genai.GenerativeModel(modelo, generation_config=my_generation_config)
     except Exception as ex:
-        print(f"Erro: {str(ex)}")
+        print(f"Erro na configuração do modelo: {str(ex)}")
         return False
+    return model
 
-def pergunte_ao_gemini(chat_configurado, prompt):
+def configura_chat(modelo:genai.GenerativeModel):
+    try:
+        chat = modelo.start_chat(history=[])
+    except Exception as ex:
+        print(f"Erro na configuração do chat: {str(ex)}")
+        return False
+    return chat
+
+def pergunte_ao_gemini(chat_configurado:genai.ChatSession, prompt:str):
     try:        
         if chat_configurado:
             response = chat_configurado.send_message(prompt, stream=True)
@@ -39,14 +49,14 @@ def pergunte_ao_gemini(chat_configurado, prompt):
 def principal():    
     prompt = input('Qual é a pergunta? ')
     try: 
-        chat_configurado, modelo_configurado = configura_modelo()
+        modelo_configurado = configura_modelo()
+        chat_configurado = configura_chat(modelo_configurado)
         while len(prompt) > 0: 
             print('Aguarde...')
             resposta = pergunte_ao_gemini(chat_configurado, prompt)  
             qtd_tokens_enviados.append(modelo_configurado.count_tokens(prompt))  
             print(resposta)
             qtd_tokens_recebidos.append(modelo_configurado.count_tokens(chat_configurado.history))
-
             prompt = input('Qual é a pergunta? ')
             if (not prompt) or (prompt.lower() == 'sair'):
                 break
